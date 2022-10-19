234a1ade4d1956580b5e079a2b84f4a8dbbbfa5d
poneciak57 <poneciak@gmail.com>
2022-10-19 13:41:19 +0200

    Changed configuration file
    changed configuration file for first deployment

--- src/main/java/com/whattobake/Api/Configuration/DatabaseConfig.java
+++ src/main/java/com/whattobake/Api/Configuration/DatabaseConfig.java
@@ -3,22 +3,27 @@ package com.whattobake.Api.Configuration;
 import dev.miku.r2dbc.mysql.MySqlConnectionConfiguration;
 import dev.miku.r2dbc.mysql.MySqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.core.DatabaseClient;
 
 @Configuration
 @EnableR2dbcRepositories
+@RequiredArgsConstructor
 public class DatabaseConfig {
+
+    private Environment env;
 
     @Bean
     public ConnectionFactory connectionFactory() {
         return MySqlConnectionFactory.from(MySqlConnectionConfiguration.builder()
                 .host("127.0.0.1")
-                .user("root")
-                .password("")
+                .user(env.getProperty("spring.r2dbc.username"))
+                .password(env.getProperty("spring.r2dbc.password"))
                 .database("whattobake")
                 .build()
         );
