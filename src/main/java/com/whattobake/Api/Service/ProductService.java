fb87387c0452d7b036c4ced4d93734961a768423
poneciak57 <poneciak@gmail.com>
2022-10-24 14:07:41 +0200

    added category mapping to ProductService

--- src/main/java/com/whattobake/Api/Service/ProductService.java
+++ src/main/java/com/whattobake/Api/Service/ProductService.java
@@ -1,18 +1,36 @@
 package com.whattobake.Api.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whattobake.Api.DTO.Bind;
+import com.whattobake.Api.Model.Category;
 import com.whattobake.Api.Model.Product;
+import com.whattobake.Api.Model.Recipe;
 import com.whattobake.Api.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
+    private final DatabaseClient databaseClient;
     private final ProductRepository productRepository;
 
     public Flux<Product> allProducts(){
-        return productRepository.findAll();
+        String query = "SELECT p.id, p.name,c.id AS c_id,c.name AS c_name FROM whattobake.product AS p , whattobake.category AS c WHERE c.id = p.category" ;
+        DatabaseClient.GenericExecuteSpec genericExecuteSpec = databaseClient.sql(query);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return genericExecuteSpec.map((row,rowMeta)->
+                Product.builder()
+                        .id(row.get("id",Long.class))
+                        .name(row.get("name",String.class))
+                        .category(new Category(row.get("c_id",Long.class),row.get("c_name",String.class)))
+                        .build()
+        ).all();
     }
 }
